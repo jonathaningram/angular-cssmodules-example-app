fea716e4886067375965ac272416582008939784
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2015-09-10 12:17:38 +1000

    Add eslint-plugin-angular
    Use controllerAs syntax for DashboardCtrl.

--- src/app/dashboard/DashboardCtrl.js
+++ src/app/dashboard/DashboardCtrl.js
@@ -1,8 +1,9 @@
 import styles from './index.scss';
 console.log('dashboard index.scss styles:', styles);
 
-DashboardCtrl.$inject = ['$rootScope', '$scope'];
+DashboardCtrl.$inject = ['$rootScope'];
 
-export default function DashboardCtrl($rootScope, $scope) {
-  $scope.styles = Object.assign({}, $rootScope.styles || {}, styles);
+export default function DashboardCtrl($rootScope) {
+  let vm = this;
+  vm.styles = Object.assign({}, $rootScope.styles || {}, styles);
 }
